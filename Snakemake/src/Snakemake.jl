6ba8b307e56ce7de1da7135a1a13298318062469
Nathan Hattersley <nate.hattersley@gmail.com>
2025-11-09 12:28:31 -0600

    Snakemake first pass

---
+++ Snakemake/src/Snakemake.jl
@@ -0,0 +1,40 @@
+module Snakemake
+
+export snakemake!, snakemake, with_logging
+
+import JSON3
+
+@kwdef struct Snakemake
+    input::Union{Dict,Vector} = Dict()
+    output::Union{Dict,Vector} = Dict()
+    params::Union{Dict,Vector} = Dict()
+    wildcards::Union{Dict,Vector} = Dict()
+    log::Union{Dict,Vector} = Dict()
+    rule::String = ""
+end
+
+snakemake = nothing
+
+function snakemake!(infile=stdin)
+    in_dict = infile == stdin && Base.isinteractive() ? Dict() : JSON3.read(infile, Dict)
+    snek = Snakemake(; (Symbol(k) => v for (k, v) in in_dict)...)
+    setproperty!(@__MODULE__, :snakemake, snek)
+    return nothing
+end
+
+__init__() = snakemake!()
+
+#### wrap logging
+import LoggingExtras
+import Logging
+
+function with_logging(f)
+    return LoggingExtras.with_logger(
+        f,
+        LoggingExtras.MinLevelLogger(
+            LoggingExtras.FileLogger(snakemake.log[1]),
+            Logging.Info,
+        ),
+    )
+end
+end
